838103e4b7dc93c1ec7928713987f4a5e472f993
tgferree <tgferree@asu.edu>
2016-02-21 17:36:11 -0700

    Update Account.java

--- CST 316 - Code Inspection Lab/given/src/banking/primitive/core/Account.java
+++ CST 316 - Code Inspection Lab/given/src/banking/primitive/core/Account.java
@@ -1,7 +1,7 @@
 package banking.primitive.core;
 
 public abstract class Account implements java.io.Serializable {
-    private static final long serialVersionUID = 1L;
+    private static final long SERIALVERSIONUID = 1L;
 
     protected enum State {
         OPEN, CLOSED, OVERDRAWN
